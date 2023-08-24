01891f4bb65be73a031f10033bbf95574b19a242
kurodenjiro <kurodenjiro1@gmail.com>
2023-08-25 05:31:28 +0700

    update

--- src/components/Button/ConnectButton/ConnectButton.tsx
+++ src/components/Button/ConnectButton/ConnectButton.tsx
@@ -1,26 +1,43 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import MenuPlaceholder from "./MenuPlaceholer";
 
 // @styled-components
 import { Layout } from "./ConnectButton.styled";
 
-// @near
-import { login } from "near/utils";
+
+import { Wallet } from "near/near-wallet"
 
 interface ConnectButtonProps {}
 
+
 const ConnectButton: React.FC<ConnectButtonProps> = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [accountId, setAccountId] = useState(false);
+  const CONTRACT_ADDRESS = "dev-1692890433237-45573031471653";
+  // When creating the wallet you can optionally ask to create an access key
+  // Having the key enables to call non-payable methods without interrupting the user to sign
+  const wallet = new Wallet({ createAccessKeyFor: CONTRACT_ADDRESS });
 
-  const isSignedIn = window?.walletConnection?.isSignedIn();
-  const accountId = window.accountId;
+ 
+ 
+  useEffect(() => {
+    const startUp = async () => {
+      const isSignedIn = await wallet.startUp();
+      setAccountId(wallet.accountId);
+      setIsSignedIn(isSignedIn);
+    };
+  
+    startUp()
+      .catch(console.error);;
+  }, [])
 
   const handleLogin = () => {
     if (isSignedIn) {
       setIsMenuOpen(!isMenuOpen); // Đảo ngược trạng thái mở/đóng khi nhấn nút "Wallet Connect"
     } else {
       setIsMenuOpen(!isMenuOpen); // Mở menu placeholder khi chưa đăng nhập và nhấn nút "Wallet Connect"
-      login(); // Thực hiện login khi nhấn nút "Wallet Connect"
+      wallet.signIn(); // Thực hiện login khi nhấn nút "Wallet Connect"
     }
   };
 
