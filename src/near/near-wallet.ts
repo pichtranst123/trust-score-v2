31990a40ee506b7d40d33dc0c55a01612378bcb0
Alexander Tran <pichstudent2004@gmail.com>
2023-08-28 05:10:46 +0700

    test

--- src/near/near-wallet.ts
+++ src/near/near-wallet.ts
@@ -3,12 +3,11 @@ import "@near-wallet-selector/modal-ui/styles.css";
 import { setupModal } from "@near-wallet-selector/modal-ui";
 import LedgerIconUrl from "@near-wallet-selector/ledger/assets/ledger-icon.png";
 import MyNearIconUrl from "@near-wallet-selector/my-near-wallet/assets/my-near-wallet-icon.png";
-import { NearWalletIconUrl } from "@near-wallet-selector/near-wallet/assets/near-wallet-icon.png";
+import  NearWalletIconUrl  from "@near-wallet-selector/near-wallet/assets/near-wallet-icon.png";
 
 import { setupWalletSelector } from "@near-wallet-selector/core";
 import { setupLedger } from "@near-wallet-selector/ledger";
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupNearWallet } from "@near-wallet-selector/near-wallet";
 
 const THIRTY_TGAS = "30000000000000";
 const NO_DEPOSIT = "0";
@@ -31,7 +30,6 @@ export class Wallet {
       modules: [
         setupMyNearWallet({ iconUrl: MyNearIconUrl as any }),
         setupLedger({ iconUrl: LedgerIconUrl as any }),
-        setupNearWallet({iconUrl: NearWalletIconUrl}),
       ],
     });
 
