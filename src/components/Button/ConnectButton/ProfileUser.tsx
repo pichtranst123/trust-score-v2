34a11e9bc4a0c0dd827b461a7bf2857f232b691c
kurodenjiro <kurodenjiro1@gmail.com>
2023-08-27 22:50:44 +0700

    fix path profile

--- src/components/Button/ConnectButton/ProfileUser.tsx
+++ src/components/Button/ConnectButton/ProfileUser.tsx
@@ -4,12 +4,14 @@ import { Layout } from "./ProfileUser.styled";
 import { BiUserCircle } from "react-icons/bi";
 // @near
 import { Wallet } from "near/near-wallet";
+import { useRouter } from 'next/router';
 
 interface ButtonLogoutProps {}
 
 const ButtonLogout: React.FC<ButtonLogoutProps> = () => {
   const contractId = process.env.NEXT_PUBLIC_CONTRACT_NAME;
   const wallet = new Wallet({ createAccessKeyFor: contractId });
+  const router = useRouter();
   useEffect(() => {
     const startUp = async () => {
       const isSignedIn = await wallet.startUp();
@@ -18,12 +20,11 @@ const ButtonLogout: React.FC<ButtonLogoutProps> = () => {
     startUp()
       .catch(console.error);;
   }, [])
-  const handleLogout = async() => {
-    await wallet.startUp();
-    await wallet.signOut();
+  const gotoProfile = async() => {
+    router.push(`/profile`);
   };
 
-  return <Layout onClick={handleLogout}><BiUserCircle />&nbsp;Profile</Layout>;
+  return <Layout onClick={gotoProfile}><BiUserCircle />&nbsp;Profile</Layout>;
 };
 
 export default ButtonLogout;
